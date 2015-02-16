8b4e03f634b3a18e11bbc655b5143ae66682871d
Kenan Yildirim <kenan@kenany.me>
2015-02-16 07:53:57 -0500

    mercury@14.0.0

--- client/index.js
+++ client/index.js
@@ -3,24 +3,70 @@ var value = require('observ');
 var struct = require('observ-struct');
 var Delegator = require('dom-delegator');
 var mainLoop = require('main-loop');
-var h = require('virtual-hyperscript');
-var MultipleEvent = require('geval/multiple');
-var changeEvent = require('value-event/change');
+var h = require('virtual-dom/virtual-hyperscript');
+var sendChange = require('value-event/change');
 var find = require('lodash.find');
+var diff = require('virtual-dom/vtree/diff');
+var patch = require('virtual-dom/vdom/patch');
+var create = require('virtual-dom/vdom/create-element');
+var extend = require('xtend');
 
 var courses = require('./catalog');
 
-var state = struct({
-  query: value(''),
-  events: MultipleEvent(['change'])
-});
+function channels(funcs, context) {
+  return Object.keys(funcs).reduce(createHandle, {});
 
-state.events.change(function(data) {
+  function createHandle(acc, name) {
+    var handle = Delegator.allocateHandle(funcs[name].bind(null, context));
+    acc[name] = handle;
+    return acc;
+  }
+}
+
+function mercury(obj) {
+  var copy = extend(obj);
+  var copyChannels = copy.channels;
+  var copyHandles = copy.handles;
+
+  if (copyChannels) {
+      copy.channels = value(null);
+  }
+  else if (copyHandles) {
+      copy.handles = value(null);
+  }
+
+  var observ = struct(copy);
+
+  if (copyChannels) {
+    observ.channels.set(channels(copyChannels, observ));
+  }
+  else if (copyHandles) {
+    observ.handles.set(channels(copyHandles, observ));
+  }
+
+  return observ;
+}
+
+function change(state, data) {
+  console.log('change');
   state.query.set(data.query);
+}
+
+var state = mercury({
+  query: value(''),
+  channels: {
+    change: change
+  }
 });
 
 Delegator();
-var loop = mainLoop(state(), render);
+
+var loop = mainLoop(state(), render, {
+  diff: diff,
+  create: create,
+  patch: patch
+});
+
 document.body.appendChild(loop.target);
 state(loop.update);
 
@@ -73,7 +119,7 @@ function render(state) {
     type: 'search',
     name: 'query',
     value: String(state.query),
-    'ev-event': changeEvent(state.events.change),
+    'ev-event': sendChange(state.channels.change),
     autofocus: true
   });
 
