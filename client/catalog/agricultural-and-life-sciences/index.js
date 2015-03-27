9037646c7fd2a0b8fff7a4ad9f336ba532a2c200
Kenan Yildirim <kenan@kenany.me>
2015-03-27 17:35:02 -0400

    Add agricultural-and-life-sciences courses

---
+++ client/catalog/agricultural-and-life-sciences/index.js
@@ -0,0 +1,21 @@
+module.exports = [
+  require('./als2410'),
+  require('./als2931'),
+  require('./als3133'),
+  require('./als3153'),
+  require('./als3203'),
+  require('./als3923'),
+  require('./als3940'),
+  require('./als4404'),
+  require('./als4419'),
+  require('./als4905'),
+  require('./als4911'),
+  require('./als4915'),
+  require('./als4921'),
+  require('./als4932'),
+  require('./als4936'),
+  require('./als4937'),
+  require('./als4938'),
+  require('./als4941'),
+  require('./als4950')
+];
